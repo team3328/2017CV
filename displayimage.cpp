49ab0276c7585d53519674cc431d2977e52e48fa
Anton <aopanis@gmail.com>
2017-03-27 21:59:29 -0700

    Updated code, not most but that will come later

--- displayimage.cpp
+++ displayimage.cpp
@@ -1,14 +1,19 @@
 # include "opencv2/opencv.hpp"
-# include "hsvoptimizer.h"
+# include <algorithm>
 # include <iostream>
 # include <fstream>
 # include "stdlib.h"
 # include "networktables/NetworkTable.h"
+# ifdef JETSON_ENV
+# include <lidarlite.h>
+# endif
 # include <chrono>
 # include <thread>
 
 using namespace cv;
 using namespace std;
+
+//ifdefs are for removing code based on whether the jetson or a desktop is running it
 
 #ifdef DESKTOP_ENV
 void on_trackbar(int, void*)
@@ -24,19 +29,23 @@ int hue_max;
 int value_min;
 int value_max;
 
-int main(int, char**)
+int main()
 {
-	hsvoptimizer::foo();
 	// open video stream
 	VideoCapture cap(0); // open the default camera
 	if(!cap.isOpened())	// check if we succeeded
 			return -1;
 
-	cap.set(CV_CAP_PROP_FRAME_WIDTH, 1280);
-	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 720);
+	cap.set(V4L2_CID_ISO_SENSITIVITY_AUTO, V4L2_CID_ISO_SENSITIVITY_MANUAL);
+	cap.set(V4L2_CID_ISO_SENSITIVITY, 80);
+	// Doesnt work do to an old version of v4l
+	// cap.set(CV_CAP_PROP_FRAME_WIDTH, 1280);
+	// cap.set(CV_CAP_PROP_FRAME_HEIGHT, 720);
 
 #ifdef JETSON_ENV
 	auto table = NetworkTable::GetTable("JetsonData");
+	LidarLite *lidarLite = new LidarLite();
+	int err = lidarLite->openLidarLite();
 #endif
 
 	Mat edges;
@@ -81,7 +90,8 @@ int main(int, char**)
 		// GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
 
 		// make all green colors white and everything else black
-		inRange(edges, Scalar(hue_min, saturation_min, value_min), Scalar(hue_max, saturation_max, value_max), dest);
+		inRange(edges, Scalar(hue_min, saturation_min, value_min),
+									 Scalar(hue_max, saturation_max, value_max), dest);
 
 		// creates kernels for erode and dilate
 		Mat element_erode = getStructuringElement(MORPH_RECT, Size(10,10), Point(0,0));
@@ -114,38 +124,53 @@ int main(int, char**)
 			// Filter out invalid rectangles
 			const int width = size.width;
 			const int height = size.height;
-			if (width < 10 || height < 10) {
+			if (width < 40 || height < 10) {
 				continue;
 			}
 
-			if (height * 3 > width) {
+			if (height * 2  > width * 1) {
 				continue;
 			}
 
+#ifdef DESKTOP_ENV
 			static const Scalar color(255,0,0);
 			rectangle(frame, rect.tl(), rect.br(), color, 2, 8, 0);
+#endif
 			// rectangle is acceptable, add it to the list
 			boundingRects.emplace_back(std::move(rect));
 		}
 
+		sort(boundingRects.begin(), boundingRects.end(), [](const Rect& rect1, const Rect& rect2) -> bool
+		{
+			return rect1.area() > rect2.area();
+		});
+
 #ifdef JETSON_ENV
-		if(boundingRects.size() > 1)
+		if (err >= 0)
+		{
+			int distance = lidarLite->getDistance();
+			if (distance < 0)
+			{
+				int llerror;
+				llerror = lidarLite->getError();
+				cout << llerror;
+			}
+			table->PutNumber("distance", distance);
+		}
+		if(boundingRects.size() > 0)
 		{
 			int x_top = (boundingRects[0].tl().x + boundingRects[0].br().x) / 2;
 			int y_top = (boundingRects[0].tl().y + boundingRects[0].br().y) / 2;
-			int x_bot = (boundingRects[1].tl().x + boundingRects[1].br().x) / 2;
-			int y_bot = (boundingRects[1].tl().y + boundingRects[1].br().y) / 2;
+			cout << x_top;
+			// int x_bot = (boundingRects[1].tl().x + boundingRects[1].br().x) / 2;
+			// int y_bot = (boundingRects[1].tl().y + boundingRects[1].br().y) / 2;
 			table->PutNumber("pixels", x_top);
 		}
 #endif
 #ifdef DESKTOP_ENV
 		// show original video stream and edited video stream
 		imshow("color", frame);
-#endif
 		if(waitKey(30) >= 0) {
-#ifdef JETSON_ENV
-			NetworkTable::Shutdown();
-#endif
       ofstream hsvValues;
       hsvValues.open("goodvalues.txt");
       hsvValues << hue_min << "\n" << hue_max << "\n" << saturation_min << "\n"
@@ -153,6 +178,7 @@ int main(int, char**)
       hsvValues.close();
 			break;
 		}
+#endif
 	}
 	// the camera will be deinitialized automatically in VideoCapture destructor
 	return 0;
